304cebab89d370ff04b2bc07715dba476355d05f
LCH-1228 <chanho1228@gmail.com>
2025-04-16 11:55:30 +0900

    feat: 연락처 목록 UI구현

---
+++ LCH-1228/nbcampContactsApp/TableViewCell/ContactsListCell.swift
@@ -0,0 +1,72 @@
+//
+//  ContactsListCell.swift
+//  nbcampContactsApp
+//
+//  Created by Chanho Lee on 4/16/25.
+//
+import UIKit
+import SnapKit
+
+class ContactsListCell: UITableViewCell {
+    
+    static let identifier = "contactListCell"
+    
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "questionmark.circle")
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 40
+        imageView.layer.borderColor = .init(red: 128/255, green: 128/255, blue: 128/255, alpha: 0.8)
+        imageView.layer.borderWidth = 2
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이름"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "010-0000-0000"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        [
+            profileImage,
+            nameLabel,
+            numberLabel
+        ].forEach{ contentView.addSubview($0) }
+        
+        profileImage.snp.makeConstraints {
+            $0.size.equalTo(80)
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(28)
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(profileImage.snp.trailing).offset(24)
+        }
+        
+        numberLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-32)
+        }
+    }
+}
---
+++ LCH-1228/nbcampContactsApp/ViewController/ContactsListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ContactsListViewController.swift
+//  nbcampContactsApp
+//
+//  Created by Chanho Lee on 4/16/25.
+//
+
+import UIKit
+import SnapKit
+
+class ContactsListViewController: UIViewController {
+    
+    private lazy var contactList: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ContactsListCell.self, forCellReuseIdentifier: ContactsListCell.identifier)
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("vewDidLoad[ViewController]")
+        configureUI()
+        configureNav()
+    }
+    
+    private func configureUI() {
+        [
+            contactList,
+        ].forEach{ view.addSubview($0) }
+        
+        contactList.snp.makeConstraints {
+            $0.size.equalToSuperview()
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    private func configureNav() {
+        navigationItem.title = "포켓몬 목록"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(showContactsDetailView))
+    }
+    
+    @objc private func showContactsDetailView() {
+        print("showContactsDetailView Button Tapped")
+    }
+}
+
+extension ContactsListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension ContactsListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContactsListCell.identifier, for: indexPath) as? ContactsListCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
