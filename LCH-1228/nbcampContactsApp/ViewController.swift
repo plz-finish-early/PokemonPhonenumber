7227449f8cf0e636e1b8b9e24e496ef68bc192ed
LCH-1228 <chanho1228@gmail.com>
2025-04-16 09:03:24 +0900

    Initial project commit

---
+++ LCH-1228/nbcampContactsApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  nbcampContactsApp
+//
+//  Created by Chanho Lee on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("vewDidLoad[ViewController]")
+    }
+}
