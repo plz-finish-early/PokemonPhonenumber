ab6d4ff9932f151179c29e165d9ac1b20d10a317
LCH-1228 <chanho1228@gmail.com>
2025-04-17 11:24:46 +0900

    feat: pokeapi 이미지 받아오기 비동기 처리 구현

--- LCH-1228/nbcampContactsApp/ViewController/ContactsDetailViewController.swift
+++ LCH-1228/nbcampContactsApp/ViewController/ContactsDetailViewController.swift
@@ -96,7 +96,7 @@ class ContactsDetailViewController: UIViewController {
                                                             action: #selector(editButtonTapped))
     }
     
-    private func fetchData<T: Decodable>(url: URL, completion: @escaping (T?) -> Void) {
+    private func fetchData<T>(url: URL, completion: @escaping (T?) -> Void) {
         let session = URLSession(configuration: .default)
         session.dataTask(with: URLRequest(url: url)) { data, response, error in
             guard let data, error == nil else {
@@ -107,12 +107,7 @@ class ContactsDetailViewController: UIViewController {
             
             let successHttpResponeRange = 200..<300
             if let response = response as? HTTPURLResponse, successHttpResponeRange.contains(response.statusCode) {
-                guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
-                    print("JSON 디코딩 실패")
-                    completion(nil)
-                    return
-                }
-                completion(decodedData)
+                completion(data as? T)
             } else {
                 print("응답 오류")
                 completion(nil)
@@ -129,16 +124,23 @@ class ContactsDetailViewController: UIViewController {
             print("url 생성 실패")
             return
         }
-        
-        fetchData(url: url) { [weak self] (result: ImageData?) in
-            guard let self, let result else {
-                print("2")
+        fetchData(url: url) { [weak self] (result: Data?) in
+            guard let self, let result else { return }
+            
+            guard let decodedData = try? JSONDecoder().decode(ImageData.self, from: result) else {
+                print("JSON 디코딩 실패")
                 return
             }
-            guard let imageUrl = URL(string: result.sprites.other.home.frontDefault) else { return }
             
-            if let data = try? Data(contentsOf: imageUrl) {
-                if let image = UIImage(data: data) {
+            guard let imageUrl = URL(string: decodedData.sprites.other.home.frontDefault) else {
+                print("이미지 url 생성 실패")
+                return
+            }
+            
+            fetchData(url: imageUrl) { [weak self] (result: Data?) in
+                guard let self, let result else { return }
+                
+                if let image = UIImage(data: result) {
                     DispatchQueue.main.async {
                         self.profileImage.image = image
                     }
@@ -147,47 +149,6 @@ class ContactsDetailViewController: UIViewController {
         }
     }
     
-    /*
-    private func fetchImageData(completion: @escaping (ImageData?) -> Void) {
-        let ramdomNumber = Int.random(in: 1...1025)
-        
-        var urlComponent = URLComponents(string: "https://pokeapi.co")
-        urlComponent?.path = "/api/v2/pokemon/\(ramdomNumber)"
-        guard let url = urlComponent?.url else {
-            print("url 생성 실패")
-            completion(nil)
-            return
-        }
-        print(url)
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        
-        let urlSession = URLSession(configuration: .default)
-        urlSession.dataTask(with: urlRequest) { data, response, error in
-            guard let data, error == nil else {
-                print("데이터 받아오기 실패")
-                completion(nil)
-                return
-            }
-            
-            let successHttpRespone = 200..<300
-            if let response = response as? HTTPURLResponse {
-                if successHttpRespone.contains(response.statusCode) {
-                    guard let decodedData = try? JSONDecoder().decode(ImageData.self, from: data) else {
-                        print("JSON 디코딩 실패")
-                        completion(nil)
-                        return
-                    }
-                    completion(decodedData)
-                }
-            } else {
-                print("응답 오류")
-                completion(nil)
-            }
-        }.resume()
-    }
-    */
     @objc private func editButtonTapped() {
         print("editButtonTapped")
     }
