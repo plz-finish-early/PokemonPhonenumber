548133df455311286deb8fb69658ef8388d32033
NH <mnh4140@gmail.com>
2025-04-18 14:16:50 +0900

    [Feat] #2 친구목록 UI 구성 완료
    - 기본 UI 구성 완료
      - UILabel, UIButton, TableView
    
    - TableViewCell UI 구성 완료
      - UIImageView, UILabel

---
+++ PokemonPhoneBook/PhoneBookTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  PhoneBookTableViewCell.swift
+//  PokemonPhoneBook
+//
+//  Created by NH on 4/18/25.
+//
+
+import UIKit
+
+class PhoneBookTableViewCell: UITableViewCell {
+    static let id = "PhoneBookTableViewCell"
+    
+    let pokemonImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .white
+        imageView.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+        imageView.layer.borderWidth = 1
+        imageView.layer.cornerRadius = 30
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "name"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let phoneNumLabel: UILabel = {
+        let label = UILabel()
+        label.text = "010-1111-2222"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        self.backgroundColor = .blue
+        contentView.backgroundColor = .green
+        
+        [
+            pokemonImageView,
+            nameLabel,
+            phoneNumLabel
+        ].forEach { contentView.addSubview($0) }
+        
+        pokemonImageView.snp.makeConstraints {
+            $0.width.height.equalTo(60)
+            $0.leading.equalToSuperview().offset(30)
+            $0.centerY.equalToSuperview()
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalTo(pokemonImageView.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
+        }
+       
+        phoneNumLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(30)
+            //$0.leading.equalTo(nameLabel.snp.trailing).offset(50)
+        }
+    }
+    
+}
--- PokemonPhoneBook/ViewController.swift
+++ PokemonPhoneBook/ViewController.swift
@@ -9,25 +9,94 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    let testLabel: UILabel = {
-       let label = UILabel()
-        label.text = "Hello"
-        label.font = .boldSystemFont(ofSize: 30)
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "친구 목록"
+        label.font = .boldSystemFont(ofSize: 20)
         label.textColor = .black
         label.textAlignment = .center
         return label
+    }()
+    
+    let addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("추가", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.titleLabel?.textAlignment = .center
+        return button
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = .cyan
+        tableView.dataSource = self
+        tableView.delegate = self
+        //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(PhoneBookTableViewCell.self, forCellReuseIdentifier: PhoneBookTableViewCell.id)
+        return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        view.addSubview(testLabel)
+        setTitleLabel()
+        setAddButton()
+        setTableView()
+    }
+    
+    private func setTitleLabel() {
+        view.addSubview(titleLabel)
         
-        testLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(200)
+        titleLabel.snp.makeConstraints {
+            $0.width.equalTo(80)
+            //$0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalToSuperview().offset(80)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    private func setAddButton() {
+        view.addSubview(addButton)
+        
+        addButton.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.height.equalTo(titleLabel.snp.height)
+            $0.top.equalToSuperview().offset(80)
+            //$0.centerY.equalTo(tableView.snp.centerY)
+            $0.trailing.equalToSuperview()
+            
+        }
+    }
+    
+    private func setTableView() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+            $0.bottom.equalToSuperview()
             $0.horizontalEdges.equalToSuperview()
         }
     }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PhoneBookTableViewCell.id, for: indexPath) as? PhoneBookTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
